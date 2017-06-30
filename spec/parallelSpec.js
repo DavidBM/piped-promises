30f5f6a7de08317a3ce499aed2ca3a965fc000b7
David Bonet Montes <webbonet@gmail.com>
2017-06-30 17:41:37 +0200

    Add limit to paralell

--- spec/parallelSpec.js
+++ spec/parallelSpec.js
@@ -68,4 +68,153 @@ describe('Parallel', function() {
 			done();
 		});
 	});
+
+	it('should call in parallel a maximun of callbacks as specified in the second argument', function(done) {
+		var executedPromises = [];
+
+		var cbFactory = (time) => {
+			return function cb() {
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						executedPromises.push(time);
+						resolve();
+					}, time);
+				});
+			};
+		};
+
+		var cb1 = cbFactory(220);
+		var cb2 = cbFactory(100);
+		var cb3 = cbFactory(50);
+
+		var cb4 = cbFactory(1);
+		var cb5 = cbFactory(60);
+		var cb6 = cbFactory(20);
+
+		Piped.parallel([cb1, cb2, cb3, cb4, cb5, cb6], 3)
+		.then(() => {
+			expect(executedPromises[0]).toBe(50);
+			expect(executedPromises[1]).toBe(1);
+			expect(executedPromises[2]).toBe(100);
+
+			expect(executedPromises[3]).toBe(60);
+			expect(executedPromises[4]).toBe(20);
+			expect(executedPromises[5]).toBe(220);
+
+			done();
+		});
+	});
+
+	it('should call all in parallel if the maximum specified is undefined', function(done) {
+		var executedPromises = [];
+
+		var cbFactory = (time) => {
+			return function cb() {
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						executedPromises.push(time);
+						resolve();
+					}, time);
+				});
+			};
+		};
+
+		var cb1 = cbFactory(120);
+		var cb2 = cbFactory(110);
+		var cb3 = cbFactory(100);
+
+		var cb4 = cbFactory(1);
+		var cb5 = cbFactory(10);
+		var cb6 = cbFactory(20);
+
+		Piped.parallel([cb1, cb2, cb3, cb4, cb5, cb6])
+		.then(() => {
+			expect(executedPromises[0]).toBe(1);
+			expect(executedPromises[1]).toBe(10);
+			expect(executedPromises[2]).toBe(20);
+
+			expect(executedPromises[3]).toBe(100);
+			expect(executedPromises[4]).toBe(110);
+			expect(executedPromises[5]).toBe(120);
+
+			done();
+		});
+	});
+
+	it('should call all in parallel if the maximum specified is undefined', function(done) {
+		var executedPromises = [];
+
+		var cbFactory = (time) => {
+			return function cb() {
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						executedPromises.push(time);
+						resolve();
+					}, time);
+				});
+			};
+		};
+
+		var cb1 = cbFactory(120);
+		var cb2 = cbFactory(110);
+		var cb3 = cbFactory(100);
+
+		var cb4 = cbFactory(1);
+		var cb5 = cbFactory(10);
+		var cb6 = cbFactory(20);
+
+		Piped.parallel([cb1, cb2, cb3, cb4, cb5, cb6])
+		.then(() => {
+			expect(executedPromises[0]).toBe(1);
+			expect(executedPromises[1]).toBe(10);
+			expect(executedPromises[2]).toBe(20);
+
+			expect(executedPromises[3]).toBe(100);
+			expect(executedPromises[4]).toBe(110);
+			expect(executedPromises[5]).toBe(120);
+
+			done();
+		});
+	});
+
+	it('should stop calling callbacks if there is a rejection', function(done) {
+		var executedPromises = [];
+
+		var cbFactory = (time, rejectPromise) => {
+			return function cb() {
+				return new Promise((resolve, reject) => {
+					setTimeout(() => {
+						executedPromises.push(time);
+
+						if(rejectPromise){
+							return reject();
+						}
+
+						resolve();
+					}, time);
+				});
+			};
+		};
+
+		var cb1 = cbFactory(10);
+		var cb2 = cbFactory(20, true);
+		var cb3 = cbFactory(30);
+
+		var cb4 = cbFactory(40);
+		var cb5 = cbFactory(50);
+		var cb6 = cbFactory(20);
+
+		Piped.parallel([cb1, cb2, cb3, cb4, cb5, cb6], 3)
+		.then(() => {})
+		.catch(() => {});
+
+		setTimeout(() => {
+			expect(executedPromises.length).toBe(4);
+			done();
+		}, 200);
+	});
+
+	it('should return an array with the result in order', function() {
+		
+	});
 });
